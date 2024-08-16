9f0be72c69eb5c343abcac3d7be1ed06fb6cf349
Your Name <you@example.com>
2024-08-16 18:25:16 +0300

    Мелкие изменение !

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -5,14 +5,16 @@ import styles from "./AddComment.module.scss";
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import { useSelector } from "react-redux";
 
 export const Index = () => {
+  const {data} = useSelector(state => state.auth)
   return (
     <>
       <div className={styles.root}>
         <Avatar
           classes={{ root: styles.avatar }}
-          src="https://mui.com/static/images/avatar/5.jpg"
+          src={data.avatarUrl}
         />
         <div className={styles.form}>
           <TextField
