8a6ab7412dcec9375ee7bb3346dd64dfe6abaa3d
Your Name <you@example.com>
2024-08-16 20:33:47 +0300

    Мелкие изменение !

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -14,7 +14,7 @@ export const Index = () => {
       <div className={styles.root}>
         <Avatar
           classes={{ root: styles.avatar }}
-          src={data?.avatarUrl}
+          src={data?.avatarUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNJqxCzhVRrUBUNpyz-e74mtn28OI9fwhLKUUApAeLyxSfN8B61bAE8G11NZanJZC2eAo&usqp=CAU'}
         />
         <div className={styles.form}>
           <TextField
