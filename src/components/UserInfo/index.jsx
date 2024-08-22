6438d65bd896b228c825de2eb7a8a0f2f5ba625d
Your Name <you@example.com>
2024-08-22 23:25:45 +0300

    first commit

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -4,7 +4,7 @@ import styles from './UserInfo.module.scss';
 export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={`${process.env.REACT_APP_API_URL}${avatarUrl}` || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNJqxCzhVRrUBUNpyz-e74mtn28OI9fwhLKUUApAeLyxSfN8B61bAE8G11NZanJZC2eAo&usqp=CAU'} alt={fullName} />
+      <img className={styles.avatar} src={ avatarUrl && `${process.env.REACT_APP_API_URL}${avatarUrl}` || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNJqxCzhVRrUBUNpyz-e74mtn28OI9fwhLKUUApAeLyxSfN8B61bAE8G11NZanJZC2eAo&usqp=CAU'} alt={fullName} />
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
         <span className={styles.additional}>{additionalText.slice(0,10).split('-').reverse().join('-') + 'г'}</span>
